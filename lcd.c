7aaa2d31f0a0008862fb4265af21035d701973c8
Tejas Prajapati <plinthtechnologies@gmail.com>
2016-03-27 17:30:02 +0530

    Changes for creating final LCD module.

--- lcd.c
+++ lcd.c
@@ -1,6 +1,33 @@
 #include<reg51.h>
 
+sbit RS = P0^0;
+sbit EN = P0^1;
+sbit D0 = P2^0;
+sbit D1 = P2^1;
+sbit D2 = P2^2;
+sbit D3 = P2^3;
+sbit D4 = P2^4;
+sbit D5 = P2^5;
+sbit D6 = P2^6;
+sbit D7 = P2^7;
+
+void delay(unsigned int)
+{
+	int x,y,a;
+	for(i=0;i<=a;i++)
+		for(j=0;j<=1275;j++);
+}
+
+void sendcommand(unsigned char)
+{
+	 p
+}
+
+
+
 void main()
 {
+	 
+  
 
 }
