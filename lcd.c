33b935e348004ba1d90985f5c86f92a6f32a6eaf
Tejas Prajapati <plinthtechnologies@gmail.com>
2016-04-03 18:27:36 +0530

    completed code and remaining run on proteas

--- lcd.c
+++ lcd.c
@@ -1,33 +1,102 @@
 #include<reg51.h>
 
+sfr lcd_port=0xA0;
+
 sbit RS = P0^0;
-sbit EN = P0^1;
-sbit D0 = P2^0;
-sbit D1 = P2^1;
-sbit D2 = P2^2;
-sbit D3 = P2^3;
-sbit D4 = P2^4;
-sbit D5 = P2^5;
-sbit D6 = P2^6;
-sbit D7 = P2^7;
+sbit RW = P0^1;
+sbit EN = P0^2;
 
-void delay(unsigned int)
+void lcd_ini();
+void lcd_command(unsigned char);
+void lcd_display(unsigned char);
+void delay_ms(unsigned int);
+
+main()
 {
-	int x,y,a;
-	for(i=0;i<=a;i++)
+
+ 	lcd_ini();
+	
+	lcd_command(0x86);
+    
+	lcd_display('D');
+	lcd_display('I');
+	lcd_display('P');
+	lcd_display('A');
+	lcd_display('K');
+
+	while(1)
+	{
+
+
+	}
+
+}
+
+void lcd_ini()
+{
+
+ 		lcd_command(0x38);
+		delay_ms(30);
+	
+		lcd_command(0x0F);
+		delay_ms(10);
+		
+		lcd_command(0x06);
+		delay_ms(10);
+	
+		lcd_command(0x01);
+		delay_ms(10);
+
+}
+
+void lcd_command(unsigned char command)
+    {
+
+		RW=0;
+		RS=0;
+		lcd_port=command;
+		EN=1;
+		EN=0;
+		delay_ms(5);
+		
+	}
+
+
+void lcd_display(unsigned char display)
+    {
+
+
+      	RW=0;
+		RS=1;
+		lcd_port=display;
+		EN=1;
+		EN=0;
+		delay_ms(1);
+		
+	
+	}
+
+void  delay_ms(unsigned int x)
+{
+
+	int i,j;
+	for(i=0;i<=x;i++)
 		for(j=0;j<=1275;j++);
-}
-
-void sendcommand(unsigned char)
-{
-	 p
-}
-
-
-
-void main()
-{
-	 
-  
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
